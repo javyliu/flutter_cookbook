31eff2dfae67eb5c9504166ad921235f45784736
qmliu <qmliu@pearlinpalm.com>
2020-12-16 10:51:21 +0800

    Complete E-Commerce App

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const jPrimaryColor = Color(0xFFFF7643);
+const jPrimaryLightColor = Color(0xFFFFECDF);
+const jPrimaryGradientColor = LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [Color(0xFFFFA53E), Color(0xFFFF7643)]);
+const jSecondaryColor = Color(0xFF979797);
+const jTextColor = Color(0xFF757575);
+const jAnimationDuration = Duration(milliseconds: 200);
