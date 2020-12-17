4f6b01d47f62d1cf0e7425ef9c53d0079253ae24
qmliu <qmliu@pearlinpalm.com>
2020-12-17 15:38:46 +0800

    move form_error to lib/components

--- lib/screens/components/sign_form.dart
+++ lib/screens/components/sign_form.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 import '../../size_config.dart';
 import 'custom_icon.dart';
-import 'form_error.dart';
+import '../../components/form_error.dart';
 
 class SignForm extends StatefulWidget {
   @override
